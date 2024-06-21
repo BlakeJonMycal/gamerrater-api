69c39cb915bf67f7ed382ad7285771646a510796
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-06-21 12:31:54 -0400

    create a game

--- raterapi/views/game_view.py
+++ raterapi/views/game_view.py
@@ -21,6 +21,38 @@ class GameView(ViewSet):
             return Response(serializer.data)
         except Game.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def create(self, request):
+        # Get the data from the client's JSON payload
+        title = request.data.get('title')
+        description = request.data.get('description')
+        designer = request.data.get('designer')
+        year_released = request.data.get('year_released')
+        number_of_players = request.data.get('number_of_players')
+        estimated_play_time = request.data.get('estimated_play_time')
+        age_recommendation = request.data.get('age_recommendation')
+
+
+
+
+        # Create a book database row first, so you have a
+        # primary key to work with
+        game = Game.objects.create(
+            user=request.user,
+            title=title,
+            description=description,
+            designer=designer,
+            year_released=year_released,
+            number_of_players=number_of_players,
+            estimated_play_time=estimated_play_time,
+            age_recommendation=age_recommendation)
+
+        # Establish the many-to-many relationships
+        category_ids = request.data.get('categories', [])
+        game.categories.set(category_ids)
+
+        serializer = GameSerializer(game, many=False)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
         
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
