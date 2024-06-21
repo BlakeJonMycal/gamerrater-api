1fddbc48580c4ce067e52858144a43aa212b6729
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-06-21 12:04:48 -0400

    game details and categories

--- raterapi/views/game_view.py
+++ raterapi/views/game_view.py
@@ -2,11 +2,10 @@ from django.http import HttpResponseServerError
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-from raterapi.models import Game
+from raterapi.models import Game, Category
 from django.contrib.auth.models import User
 
 class GameView(ViewSet):
-    """Rock view set"""
 
 
 
@@ -14,12 +13,24 @@ class GameView(ViewSet):
         games = Game.objects.all()
         serializer = GameSerializer(games, many=True)
         return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        try:
+            game = Game.objects.get(pk=pk)
+            serializer = GameSerializer(game)
+            return Response(serializer.data)
+        except Game.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         
-
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
 
 class GameSerializer(serializers.ModelSerializer):
-    """JSON serializer"""
+    categories = CategorySerializer(many=True, read_only=True)
+
 
     class Meta:
         model = Game
-        fields = ( 'id', 'title', 'description', 'designer', 'year_released', 'number_of_players', 'estimated_play_time', 'age_recommendation'  )
+        fields = ( 'id', 'title', 'description', 'designer', 'year_released', 'number_of_players', 'estimated_play_time', 'age_recommendation', 'categories'  )
